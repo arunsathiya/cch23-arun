5bf67c48a62c670c8857a6b819242da9497713a1
Arun <arun@arun.blog>
2023-12-28 19:21:04 -0800

    Finish -1 tasks

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-use axum::{routing::get, Router};
+use axum::{
+    http::{Response, StatusCode},
+    response::IntoResponse,
+    routing::get,
+    Router,
+};
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn minus_one() -> Response<String> {
+    Response::builder()
+        .status(500)
+        .body("-1".to_string())
+        .unwrap()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .route("/-1/error", get(minus_one));
 
     Ok(router.into())
 }
